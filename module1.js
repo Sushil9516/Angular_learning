365bde49544a7de5fe7f00e97d74fde97b852ea6
sushil <sonisushil9516@gmail.com>
2025-12-21 23:32:35 +0530

    21/12/2025

--- module1.js
+++ module1.js
@@ -15,9 +15,11 @@ app.config(function ($routeProvider, $locationProvider) {
     
     .otherwise({ redirectTo: '/home' });
 });
+
 app.controller('HomeController', function ($scope) {
   $scope.msg = "Home view loaded successfully";
 });
+
 app.controller('UserController', function ($scope) {
   $scope.name = '';
 });
