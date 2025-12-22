4c81740bd814a475252cc858cd6b6e983b0aeacf
Sushil Kumar Soni <sonisushil9516@gmail.com>
2025-12-22 23:24:15 +0530

    22/12/2025

--- app.js
+++ app.js
@@ -6,8 +6,5 @@ var app = angular.module("myApp", []);
 // 2. Define a Controller for the module.
 // This name ('MyController') MUST match the ng-controller="MyController" attribute in index.html.
 app.controller("MyController", function ($scope) {
-  // The $scope object is the bridge between the Controller and the View (HTML).
-  // We initialize the variable used for the ng-model="userName" input field.
-
   $scope.userName = "Bro";
 });
